58949fd1379de19e936d3519052c04e5b20ad21f
benjamin_ti <b.tisler@gmx.de>
2020-09-22 09:26:49 +0000

    Update Beagle GPIO Simple Example
    git-svn-id: https://cvsvn/svn/raspi/trunk@26 50237d99-7d21-444e-87dd-016511c29f8c

--- beagle/GPIO_40toggleP9_Pin14.sh
+++ beagle/GPIO_40toggleP9_Pin14.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "40" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio40/direction
+# echo "60" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio60/direction
 
 while true
 do
-	echo 1 > /sys/class/gpio/gpio40/value
+	echo 1 > /sys/class/gpio/gpio60/value
 	sleep 1
-	echo 0 > /sys/class/gpio/gpio40/value
+	echo 0 > /sys/class/gpio/gpio60/value
 	sleep 1
 done

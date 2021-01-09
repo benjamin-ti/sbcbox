bcfb6268ec804b39a3cd03aab642cee1afe82380
benjamin_ti <b.tisler@gmx.de>
2021-01-09 17:26:56 +0000

    BB-Buildroot: Patched-Configuration falls meine neuen SGX-Treiber-Packages nicht funktionieren
    git-svn-id: https://cvsvn/svn/raspi/trunk@50 50237d99-7d21-444e-87dd-016511c29f8c

--- beagle/buildroot/bbbrcfg/init-patched.sh
+++ beagle/buildroot/bbbrcfg/init-patched.sh
@@ -12,4 +12,4 @@ rm -rf ../buildroot-2020.02.8_patched/package/ti-sgx-km
 rm -rf ../buildroot-2020.02.8_patched/package/ti-sgx-um
 cp -R ../bbbrcfg/br-patches/ti-sgx-km ../buildroot-2020.02.8_patched/package/ti-sgx-km
 cp -R ../bbbrcfg/br-patches/ti-sgx-um ../buildroot-2020.02.8_patched/package/ti-sgx-um
-make BR2_EXTERNAL=../bbbrcfg BR2_DEFCONFIG=../bbbrcfg/config/bbb_defconfig -C ../buildroot-2020.02.8_patched/ O=$(pwd) defconfig
+make BR2_EXTERNAL=../bbbrcfg BR2_DEFCONFIG=../bbbrcfg/config/bbb_patched_defconfig -C ../buildroot-2020.02.8_patched/ O=$(pwd) defconfig

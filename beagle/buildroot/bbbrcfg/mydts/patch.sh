7b655002ffac28b12a4afcd28b961eadfa2a1ea6
benjamin_ti <b.tisler@gmx.de>
2021-01-22 18:04:33 +0000

    BB-Buildroot: SPI0-Support
    git-svn-id: https://cvsvn/svn/raspi/trunk@59 50237d99-7d21-444e-87dd-016511c29f8c

---
+++ beagle/buildroot/bbbrcfg/mydts/patch.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+diff -U 3 -H -d -r -N -- /home/user/workspaceCpp/bbbrorg/build/linux-custom/arch/arm/boot/dts/BB-SPIDEV0.dtsi /home/user/workspaceCpp/bbbrout3/build/linux-custom/arch/arm/boot/dts/BB-SPIDEV0.dtsi

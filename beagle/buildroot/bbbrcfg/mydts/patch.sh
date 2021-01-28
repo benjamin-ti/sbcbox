6fc19a71f2427d2e6aef69b219ea6ae05500c86e
benjamin_ti <b.tisler@gmx.de>
2021-01-28 10:08:29 +0000

    BB-Buildroot: Dynamisches Umschalten zwischen UART und SPI-Funktionen bei P9_21 und P9_22
    git-svn-id: https://cvsvn/svn/raspi/trunk@60 50237d99-7d21-444e-87dd-016511c29f8c

--- beagle/buildroot/bbbrcfg/mydts/patch.sh
+++ beagle/buildroot/bbbrcfg/mydts/patch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-diff -U 3 -H -d -r -N -- /home/user/workspaceCpp/bbbrorg/build/linux-custom/arch/arm/boot/dts/BB-SPIDEV0.dtsi /home/user/workspaceCpp/bbbrout3/build/linux-custom/arch/arm/boot/dts/BB-SPIDEV0.dtsi
+diff -U 3 -H -d -r -N -- /home/user/workspaceCpp/bbbrorg/build/linux-custom/arch/arm/boot/dts/am335x-uart2-spi0-univ.dtsi /home/user/workspaceCpp/bbbrout3/build/linux-custom/arch/arm/boot/dts/am335x-uart2-spi0-univ.dtsi

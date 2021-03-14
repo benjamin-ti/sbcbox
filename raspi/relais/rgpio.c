d2618042425ced0cacadde58b1e09602660b9f68
benjamin_ti <b.tisler@gmx.de>
2021-03-14 22:48:51 +0000

    Raspi: Update Relais, jetzt mit libgpiod
    git-svn-id: https://cvsvn/svn/raspi/trunk@67 50237d99-7d21-444e-87dd-016511c29f8c

--- raspi/relais/rgpio.c
+++ raspi/relais/rgpio.c
@@ -1,8 +1,57 @@
-// #include <gpiod.h>
+#include <gpiod.h>
 #include <stdio.h>
+#include <unistd.h> // usleep
+
+#ifndef CONSUMER
+#define CONSUMER "rgpio"
+#endif
 
 int main(void)
 {
-    printf("Hello\n");
+    char *chipname = "gpiochip0";
+    unsigned int line_num = 18; // GPIO Pin #18
+    struct gpiod_chip *chip;
+    struct gpiod_line *line;
+    int i, ret;
+
+    chip = gpiod_chip_open_by_name(chipname);
+    if (!chip) {
+        perror("Open chip failed\n");
+        goto end;
+    }
+
+    line = gpiod_chip_get_line(chip, line_num);
+    if (!line) {
+        perror("Get line failed\n");
+        goto close_chip;
+    }
+
+    ret = gpiod_line_request_output(line, CONSUMER, 0);
+    if (ret < 0) {
+        perror("Request line as output failed\n");
+        goto release_line;
+    }
+
+    while(1) {
+        ret = gpiod_line_set_value(line, 0);
+        if (ret < 0) {
+            perror("Request line as output failed\n");
+            goto release_line;
+        }
+        usleep(500*1000);
+        ret = gpiod_line_set_value(line, 1);
+        if (ret < 0) {
+            perror("Request line as output failed\n");
+            goto release_line;
+        }
+        usleep(500*1000);
+    }
+
+
+release_line:
+    gpiod_line_release(line);
+close_chip:
+    gpiod_chip_close(chip);
+end:
     return 0;
 }

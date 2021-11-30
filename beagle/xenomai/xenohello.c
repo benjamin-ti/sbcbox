b45b89f9fca97622a528fc77b94110f4cc266d24
benjamin_ti <b.tisler@gmx.de>
2021-11-30 17:28:54 +0000

    Beagle Xenomai Hello World RT Task
    git-svn-id: https://cvsvn/svn/raspi/trunk@109 50237d99-7d21-444e-87dd-016511c29f8c

--- beagle/xenomai/xenohello.c
+++ beagle/xenomai/xenohello.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include <alchemy/task.h>
+
+RT_TASK hello_task;
+
+// function to be executed by task
+void hello_task_func(void *arg)
+{
+    RT_TASK_INFO curtaskinfo;
+
+    rt_printf("Hello World!\n");
+
+    // inquire current task
+    rt_task_inquire(NULL, &curtaskinfo);
+
+    // print task name
+    rt_printf("Task name : %s \n", curtaskinfo.name);
+}
 
 int main(int argc, char *argv[])
 {
-    rt_printf("Hello RT world\n");
+    char  str[10];
+
+    rt_printf("Start RT Task\n");
+
+    sprintf(str, "hello");
+
+    /* Create task
+     * Arguments: &task,
+     *            name,
+     *            stack size (0=default),
+     *            priority,
+     *            mode (FPU, start suspended, ...)
+     */
+    rt_task_create(&hello_task, str, 0, 50, 0);
+
+    /*  Start task
+     * Arguments: &task,
+     *            task function,
+     *            function argument
+     */
+    rt_task_start(&hello_task, &hello_task_func, 0);
+
     return 0;
 }
 

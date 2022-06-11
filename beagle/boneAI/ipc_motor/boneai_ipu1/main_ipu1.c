52e8179afbb463f2042c8766e43a5f2f44d62eaf
benjamin_ti <b.tisler@gmx.de>
2022-06-11 18:30:41 +0200

    IPC-Motor: First simple message from host to ipu1

--- beagle/boneAI/ipc_motor/boneai_ipu1/main_ipu1.c
+++ beagle/boneAI/ipc_motor/boneai_ipu1/main_ipu1.c
@@ -1,8 +1,80 @@
+#include <xdc/runtime/Error.h>
 #include <xdc/runtime/Log.h>
+#include <ti/ipc/MessageQ.h>
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Task.h>
+
+#define IPU1MSGQNAME "Ipu1:MsgQ:1"
+
+typedef struct {
+    MessageQ_MsgHeader  reserved;
+    UInt32              cmd;
+} MyMsg;
+
+
+Void smain(UArg arg0, UArg arg1)
+{
+    int status;
+    MessageQ_Params msgqParams;
+    MessageQ_Handle ipu1msgQ;
+    MyMsg*          pMyMsg;
+
+    Log_info0("--> smain:");
+
+    MessageQ_Params_init(&msgqParams);
+    ipu1msgQ = MessageQ_create(IPU1MSGQNAME, &msgqParams);
+    if (ipu1msgQ == NULL) {
+        Log_info0("MessageQ_create failed");
+        goto main_end;
+    }
+
+    status = MessageQ_get(ipu1msgQ, (MessageQ_Msg*)&pMyMsg,
+        MessageQ_FOREVER);
+    if (status < 0) {
+        Log_info1("MessageQ_get failed: %i", status);
+        goto main_end;
+    }
+
+    /* process the message */
+    Log_info1("Msg: cmd=0x%x", pMyMsg->cmd);
+
+    status = MessageQ_free((MessageQ_Msg)pMyMsg);
+    if (status < 0) {
+        Log_info1("MessageQ_free failed: %i", status);
+    }
+
+    status = MessageQ_delete(&ipu1msgQ);
+    if (status < 0) {
+        Log_info1("MessageQ_delete failed: %i", status);
+    }
+
+main_end:
+    Log_info0("<-- smain:");
+}
 
 Int main(Int argc, Char* argv[])
 {
+    Error_Block     eb;
+    Task_Params     taskParams;
+
     Log_info0("--> main:");
 
+    Error_init(&eb);
+
+    Task_Params_init(&taskParams);
+    taskParams.instance->name = "smain";
+    taskParams.arg0 = (UArg)argc;
+    taskParams.arg1 = (UArg)argv;
+    taskParams.stackSize = 0x1000;
+    Task_create(smain, &taskParams, &eb);
+
+    if (Error_check(&eb)) {
+        Log_info0("main: failed to create application startup thread");
+    }
+
+    BIOS_start();
+
     Log_info0("<-- main:");
+
+    return 0;
 }
--- beagle/boneAI/ipc_motor/host/main.c
+++ beagle/boneAI/ipc_motor/host/main.c
@@ -1,8 +1,67 @@
 #include <stdio.h>
 
+#include <ti/ipc/Std.h>
+#include <ti/ipc/Ipc.h>
+#include <ti/ipc/transports/TransportRpmsg.h>
+#include <ti/ipc/MessageQ.h>
+
+#include <ti/ipc/MultiProc.h>
+#include <ti/ipc/Notify.h>
+
+// Don't forget to start as root (or with sudo) !!!
+
+#define IPU1MSGQNAME "Ipu1:MsgQ:1"
+#define MSGHEAPID 0
+
+typedef struct {
+    MessageQ_MsgHeader  reserved;
+    UInt32              cmd;
+} MyMsg;
+
 int main(int argc, char* argv[])
 {
+    int status;
+
+    MessageQ_QueueId    ipu1msgQ = MessageQ_INVALIDMESSAGEQ;
+    MyMsg*              pMyMsg;
+    int i;
+
     printf("--> main:\n");
+
+    Ipc_transportConfig(&TransportRpmsg_Factory);
+    status = Ipc_start();
+    if (status < 0) {
+        printf("Ipc_start failed: status = %d\n", status);
+        return status;
+    }
+
+    pMyMsg = (MyMsg*)MessageQ_alloc(MSGHEAPID, sizeof(MyMsg));
+    if (pMyMsg == NULL) {
+        printf("MessageQ_alloc failed\n");
+        goto main_end;
+    }
+
+    i = 0;
+    do {
+        status = MessageQ_open(IPU1MSGQNAME, &ipu1msgQ);
+        sleep(1);
+        i++;
+    } while (status==MessageQ_E_NOTFOUND && i<10);
+    if (status < 0) {
+        printf("MessageQ_open failed\n");
+        goto main_end;
+    }
+
+    pMyMsg->cmd = 5;
+    MessageQ_put(ipu1msgQ, (MessageQ_Msg)pMyMsg);
+
+    status = MessageQ_close(&ipu1msgQ);
+    if (status < 0) {
+        printf("MessageQ_close failed\n");
+    }
+
+main_end:
+    Ipc_stop();
 
     printf("<-- main:\n");
 

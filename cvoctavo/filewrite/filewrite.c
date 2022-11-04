cf1c260e45e851eb362a143e19f53d6d755beb2f
benjamin_ti <b.tisler@gmx.de>
2022-11-04 17:36:44 +0100

    filewrite with options

--- cvoctavo/filewrite/filewrite.c
+++ cvoctavo/filewrite/filewrite.c
@@ -1,3 +1,4 @@
+/*=========================================================================*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,11 +6,20 @@
 #include <unistd.h>
 #include <time.h>
 
-// #define TEST_BUF_SIZE 1024*1014
-#define TEST_BUF_SIZE 10*1014*1024
+#define DEFAULT_FILESIZE_MB 1
+#define DEFAULT_BUFSIZE_KB 1
+#define DEFAULT_SLEEP_US 1000
+#define DEFAULT_RUNS 1000000
 #define TEST_FILENAME "filewrite_buf.bin"
 
-int doWrite(char* pcBuf, unsigned int uiBufSize, char cPattern, char* pcBufFileName)
+static unsigned int lk_uiFileSize = DEFAULT_FILESIZE_MB*1024*1024;
+static unsigned int lk_uiBufSize  = DEFAULT_BUFSIZE_KB*1024;
+static unsigned int lk_uiSleep_ns  = DEFAULT_SLEEP_US*1000;
+static unsigned int lk_uiRuns  = DEFAULT_RUNS;
+/*=========================================================================*/
+
+/*=========================================================================*/
+static int doWrite(char* pcBuf, unsigned int uiBufSize, char cPattern, char* pcBufFileName)
 {
     FILE* fp = NULL;
     char* pc;
@@ -30,7 +40,7 @@ int doWrite(char* pcBuf, unsigned int uiBufSize, char cPattern, char* pcBufFileN
     iSize = uiBufSize;
     do
     {
-        iWrite = 1024;
+        iWrite = lk_uiBufSize;
         if (iSize < iWrite)
             iWrite = iSize;
         i = fwrite(pc, 1, iWrite, fp);
@@ -39,7 +49,7 @@ int doWrite(char* pcBuf, unsigned int uiBufSize, char cPattern, char* pcBufFileN
 
         struct timespec t;
         t.tv_sec = 0;
-        t.tv_nsec = 1*1000*1000;
+        t.tv_nsec = lk_uiSleep_ns;
         nanosleep(&t, NULL);
 
     } while(i>0);
@@ -48,7 +58,8 @@ int doWrite(char* pcBuf, unsigned int uiBufSize, char cPattern, char* pcBufFileN
     return 0;
 }
 
-int doRead(char* pcBuf, unsigned int uiBufSize, char cPattern, char* pcBufFileName)
+/*=========================================================================*/
+static int doRead(char* pcBuf, unsigned int uiBufSize, char cPattern, char* pcBufFileName)
 {
     FILE* fp = NULL;
     char* pc;
@@ -71,7 +82,7 @@ int doRead(char* pcBuf, unsigned int uiBufSize, char cPattern, char* pcBufFileNa
 
         struct timespec t;
         t.tv_sec = 0;
-        t.tv_nsec = 1*1000*1000;
+        t.tv_nsec = lk_uiSleep_ns;
         nanosleep(&t, NULL);
 
     } while(i>0);
@@ -87,7 +98,8 @@ int doRead(char* pcBuf, unsigned int uiBufSize, char cPattern, char* pcBufFileNa
     return 0;
 }
 
-int doTest(char* pcBuf, unsigned int uiBufSize, char cPattern)
+/*=========================================================================*/
+static int doTest(char* pcBuf, unsigned int uiBufSize, char cPattern)
 {
     int iErr;
     iErr = doWrite(pcBuf, uiBufSize, cPattern, TEST_FILENAME);
@@ -101,43 +113,121 @@ int doTest(char* pcBuf, unsigned int uiBufSize, char cPattern)
     return 0;
 }
 
-int main(void)
+/*=========================================================================*/
+void Usage(char *programName)
 {
+    fprintf(stdout,"%s usage:\n\n",programName);
+    // Modify here to add your usage message when the program is
+    // called without arguments
+    printf("-fFILESIZE:  File size in MB   (Defaults to %u)\n", DEFAULT_FILESIZE_MB);
+    printf("-bBUFSIZE:   Buffer size in KB (Defaults to %u)\n", DEFAULT_BUFSIZE_KB);
+    printf("-sSLEEP:     Sleep time in us  (Defaults to %u)\n", DEFAULT_SLEEP_US);
+    printf("-rRUNS:      Number of runs    (Defaults to %u)\n", DEFAULT_RUNS);
+    printf("\nExample: Call '%s -f10' for using file size of 10 MB.\n", programName);
+    printf("Note: There should be no blank between '-f' and '10'!\n");
+}
 
-    unsigned int uiBufSize = TEST_BUF_SIZE;
+
+/*=========================================================================*/
+// returns the index of the first argument that is not an option; i.e.
+// does not start with a dash or a slash
+static int HandleOptions(int argc, char *argv[])
+{
+    int i,firstnonoption=0;
+
+    for (i=1; i< argc;i++) {
+        if (argv[i][0] == '/' || argv[i][0] == '-') {
+            switch (argv[i][1]) {
+                // An argument -? means help is requested
+                case '?':
+                    Usage(argv[0]);
+                    exit(0);
+                case 'h':
+                case 'H':
+                    if (!strcmp(argv[i]+1,"help")) {
+                        Usage(argv[0]);
+                        exit(0);
+                    }
+                    else {
+                        Usage(argv[0]);
+                        exit(0);
+                    }
+                    // If the option -h means anything else
+                    // in your application add code here
+                    // Note: this falls through to the default
+                    // to print an "unknow option" message
+                // add your option switches here
+                case 'f':
+                    lk_uiFileSize = atoi(argv[i]+2)*1024*1024;
+                    break;
+                case 'b':
+                    lk_uiBufSize = atoi(argv[i]+2)*1024;
+                    break;
+                case 's':
+                    lk_uiSleep_ns = atoi(argv[i]+2)*1000;
+                    break;
+                case 'r':
+                    lk_uiRuns = atoi(argv[i]+2);
+                    break;
+                default:
+                    fprintf(stdout, "unknown option %s\n", argv[i]);
+                    exit(-1);
+            }
+        }
+        else {
+            firstnonoption = i;
+            break;
+        }
+    }
+    return firstnonoption;
+}
+
+/*=========================================================================*/
+int main(int argc, char *argv[])
+{
     char* pcBuf;
     int iErr, i;
 
-    pcBuf = malloc(uiBufSize);
+    if (argc == 1)
+    {
+        // No arguments
+    }
+    else
+    {
+        // handle the program options
+        HandleOptions(argc,argv);
+    }
 
-    for (i=0; i<100000; i++)
+    pcBuf = malloc(lk_uiFileSize);
+
+    for (i=0; i<lk_uiRuns; i++)
     {
         printf("%i\n", i);
-        iErr = doTest(pcBuf, uiBufSize, 0xAA);
+        iErr = doTest(pcBuf, lk_uiFileSize, 0xAA);
         if (iErr != 0)
             return iErr;
-        iErr = doTest(pcBuf, uiBufSize, 0x55);
+        iErr = doTest(pcBuf, lk_uiFileSize, 0x55);
         if (iErr != 0)
             return iErr;
-        iErr = doTest(pcBuf, uiBufSize, 0xA5);
+        iErr = doTest(pcBuf, lk_uiFileSize, 0xA5);
         if (iErr != 0)
             return iErr;
-        iErr = doTest(pcBuf, uiBufSize, 0x5A);
+        iErr = doTest(pcBuf, lk_uiFileSize, 0x5A);
         if (iErr != 0)
             return iErr;
-        iErr = doTest(pcBuf, uiBufSize, 0xFF);
+        iErr = doTest(pcBuf, lk_uiFileSize, 0xFF);
         if (iErr != 0)
             return iErr;
-        iErr = doTest(pcBuf, uiBufSize, 0x00);
+        iErr = doTest(pcBuf, lk_uiFileSize, 0x00);
         if (iErr != 0)
             return iErr;
-        iErr = doTest(pcBuf, uiBufSize, 0x4D);
+        iErr = doTest(pcBuf, lk_uiFileSize, 0x4D);
         if (iErr != 0)
             return iErr;
 
         struct timespec t;
         t.tv_sec = 0;
-        t.tv_nsec = 100*1000*1000;
+        t.tv_nsec = lk_uiSleep_ns;
         nanosleep(&t, NULL);
     }
 

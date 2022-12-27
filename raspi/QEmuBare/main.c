f9e53b4ce262af38ab3c64a9ffbf7ad124d28caf
benjamin_ti <b.tisler@gmx.de>
2022-12-27 09:23:05 +0100

    QEmuBare on VersatilePB now with running timer

--- raspi/QEmuBare/main.c
+++ raspi/QEmuBare/main.c
@@ -1,5 +1,68 @@
+#include <stdio.h>
+
+// -----------------------------------------------------------------------------
+
 volatile unsigned int * const UART0DR = (unsigned int *)0x101f1000;
 
+// -----------------------------------------------------------------------------
+
+#define TIMER0_BASE 0x101E2000
+
+#define TIMER0_LOAD  ((volatile unsigned int *)(TIMER0_BASE + 0x0))
+#define TIMER0_VALUE ((volatile unsigned int *)(TIMER0_BASE + 0x4))
+#define TIMER0_CONTROL ((volatile unsigned int *)(TIMER0_BASE + 0x8))
+#define TIMER0_INTCLR ((volatile unsigned int *)(TIMER0_BASE + 0xC))
+
+#define TIMER_EN 0x80
+#define TIMER_PERIODIC 0x40
+#define TIMER_INTEN 0x20
+#define TIMER_32BIT 0x02
+#define TIMER_ONESHOT 0x01
+
+// -----------------------------------------------------------------------------
+
+#define PIC_BASE 0x10140000
+#define PIC_IntStatus ((volatile unsigned int *)(PIC_BASE + 0x00))
+#define PIC_IntEnable ((volatile unsigned int *)(PIC_BASE + 0x10))
+#define PIC_IntEnClear ((volatile unsigned int *)(PIC_BASE + 0x14))
+
+#define PIC_TIMER0_1 (1 << 4)
+
+// -----------------------------------------------------------------------------
+
+void print_uart0(const char *s);
+
+// -----------------------------------------------------------------------------
+
+void __attribute__((interrupt)) irq_handler()
+{
+	print_uart0("irq_handler\n");
+//	*TIMER0_LOAD = 0xFFFF;
+//	*PIC_IntEnClear = PIC_TIMER0_1;
+//	*PIC_IntEnable = PIC_TIMER0_1;
+	*TIMER0_INTCLR = PIC_TIMER0_1;
+}
+
+/* all other handlers are infinite loops */
+void __attribute__((interrupt)) undef_handler(void) { for(;;); }
+void __attribute__((interrupt)) swi_handler(void) { for(;;); }
+void __attribute__((interrupt)) prefetch_abort_handler(void) { for(;;); }
+void __attribute__((interrupt)) data_abort_handler(void) { for(;;); }
+void __attribute__((interrupt)) fiq_handler(void) { for(;;); }
+
+// -----------------------------------------------------------------------------
+void copy_vectors(void)
+{
+	extern uint32_t vectors_start;
+	extern uint32_t vectors_end;
+	uint32_t *vectors_src = &vectors_start;
+	uint32_t *vectors_dst = (uint32_t *)0;
+
+	while (vectors_src < &vectors_end)
+		*vectors_dst++ = *vectors_src++;
+}
+
+// -----------------------------------------------------------------------------
 void print_uart0(const char *s)
 {
     while(*s != '\0') { // Loop until end of string
@@ -8,7 +71,25 @@ void print_uart0(const char *s)
     }
 }
 
-void c_entry()
+// -----------------------------------------------------------------------------
+#define BUF_SIZE 128
+
+void main()
 {
-    print_uart0("Hello world!\n");
+	char pcBuf[BUF_SIZE];
+	int i;
+	unsigned int uiTimerValue;
+
+	*TIMER0_LOAD = 0xFFFF;
+	*TIMER0_CONTROL = TIMER_EN | TIMER_PERIODIC | 0x4 | TIMER_INTEN;
+	*PIC_IntEnClear = PIC_TIMER0_1;
+	*PIC_IntEnable = PIC_TIMER0_1;
+
+	for (;;) {
+		uiTimerValue = *TIMER0_VALUE;
+		if (uiTimerValue>0x9999 && uiTimerValue<0xAAAA) {
+			snprintf(pcBuf, BUF_SIZE, "Hello world: %x\n", uiTimerValue);
+			print_uart0(pcBuf);
+		}
+	}
 }

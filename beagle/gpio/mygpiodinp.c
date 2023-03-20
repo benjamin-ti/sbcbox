923da166e8ccafcc838ef07e0f0e003afbad3c79
benjamin_ti <b.tisler@gmx.de>
2023-03-20 18:08:39 +0100

    GPIO-Input Example improved

--- beagle/gpio/mygpiodinp.c
+++ beagle/gpio/mygpiodinp.c
@@ -9,15 +9,25 @@
 int main(void)
 {
     char *chipname = "gpiochip1";
+    char *chipname2 = "gpiochip2";
     unsigned int line_num = 28; // P9_12 - GPIO_60
+    unsigned int line_num_step = 9;   // LCD_DATA3 / P8_44 / GPIO73
     struct timespec ts = { 1, 0 };
     struct gpiod_line_event event;
     struct gpiod_chip *chip;
+    struct gpiod_chip *chip2;
     struct gpiod_line *line;
+    struct gpiod_line *lineStep;
     int i, ret = 0;
     int val;
 
     chip = gpiod_chip_open_by_name(chipname);
+    if (!chip) {
+        perror("Open chip failed\n");
+        goto end;
+    }
+
+    chip2 = gpiod_chip_open_by_name(chipname2);
     if (!chip) {
         perror("Open chip failed\n");
         goto end;
@@ -37,6 +47,18 @@ int main(void)
         goto release_line;
     }
 
+    lineStep = gpiod_chip_get_line(chip2, line_num_step);
+    if (!lineStep) {
+        perror("Get lineStep failed\n");
+        goto close_chip;
+    }
+
+    ret = gpiod_line_request_output(lineStep, CONSUMER, 0);
+    if (ret < 0) {
+        perror("Request lineStep as output failed\n");
+        goto release_line;
+    }
+
     /* Notify event up to 20 times */
     i = 0;
     while (i <= 20) {
@@ -46,6 +68,11 @@ int main(void)
             ret = -1;
             goto release_line;
         } else if (ret == 0) {
+            ret = gpiod_line_set_value(lineStep, 0);
+            if (ret < 0) {
+                perror("Set lineStep failed\n");
+                goto release_line;
+            }
             printf("Wait event notification on line #%u timeout\n", line_num);
             continue;
         }
@@ -58,6 +85,12 @@ int main(void)
             goto release_line;
         }
 
+        ret = gpiod_line_set_value(lineStep, 1);
+        if (ret < 0) {
+            perror("Set lineStep failed\n");
+            goto release_line;
+        }
+
         i++;
     }
 
@@ -65,8 +98,10 @@ int main(void)
 
 release_line:
     gpiod_line_release(line);
+    gpiod_line_release(lineStep);
 close_chip:
     gpiod_chip_close(chip);
+    gpiod_chip_close(chip2);
 end:
     return ret;
 }

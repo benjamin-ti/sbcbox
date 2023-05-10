10d408c8fd91403bb82c680c91edf81fd7a47a33
benjamin_ti <b.tisler@gmx.de>
2023-05-10 10:38:50 +0200

    beagle baremetal: No running with interrupts in JTAG

--- beagle/baremetal/main.c
+++ beagle/baremetal/main.c
@@ -1,34 +1,79 @@
-#define GPIO1_BASE		0x4804C000
+#define CM_DPLL_BASE          0x44E00500
+#define CM_DPLL_CLKSEL_TIMER2 (volatile unsigned*)(CM_DPLL_BASE+0x8)
+#define TCLKIN    0x0
+#define CLK_M_OSC 0x1
+#define CLK_32KHZ 0x2
 
 #define CM_PER_BASE		0x44e00000
-#define CM_PER_GPIO1    0xAC
+#define CM_PER_L3S      (volatile unsigned*)(CM_PER_BASE+0x04)
+#define CM_PER_TIMER2   (volatile unsigned*)(CM_PER_BASE+0x80)
+#define CM_PER_GPIO1    (volatile unsigned*)(CM_PER_BASE+0xAC)
 
+#define INTCPS_BASE     0x48200000
+#define INTC_CONTROL    (volatile unsigned*)(INTCPS_BASE+0x48)
+#define INTC_MIR2       (volatile unsigned*)(INTCPS_BASE+0xC4)
+#define INTC_MIR_CLEAR2 (volatile unsigned*)(INTCPS_BASE+0xC8)
+
+#define DMTIMER2_BASE          0x48040000
+#define DMTIMER2_EOI           (volatile unsigned*)(DMTIMER2_BASE+0x20)
+#define DMTIMER2_IRQSTATUS     (volatile unsigned*)(DMTIMER2_BASE+0x28)
+#define DMTIMER2_IRQENABLE_SET (volatile unsigned*)(DMTIMER2_BASE+0x2C)
+#define DMTIMER2_IRQENABLE_CLR (volatile unsigned*)(DMTIMER2_BASE+0x30)
+#define DMTIMER2_TCLR          (volatile unsigned*)(DMTIMER2_BASE+0x38)
+#define DMTIMER2_TCRR          (volatile unsigned*)(DMTIMER2_BASE+0x3C)
+
+#define GPIO1_BASE		   0x4804C000
+#define GPIO1_OE           (volatile unsigned*)(GPIO1_BASE+0x134)
+#define GPIO1_CLEARDATAOUT (volatile unsigned*)(GPIO1_BASE+0x190)
+#define GPIO1_SETDATAOUT   (volatile unsigned*)(GPIO1_BASE+0x194)
+
+// -----------------------------------------------------------------------------
+void __attribute__((interrupt)) irq_handler()
+{
+	static int b;
+
+    *DMTIMER2_IRQSTATUS = 0x2; // Ack DMTimer2 Int
+
+    if (b) {
+        *GPIO1_SETDATAOUT = (0xF<<21);
+    }
+    else {
+    	*GPIO1_CLEARDATAOUT = (0xF<<21);
+    }
+    b = !b;
+
+    *DMTIMER2_TCRR = 0xFF000000;
+    *DMTIMER2_TCLR = 0x1;
+
+    *INTC_CONTROL = 0x1; // Ack Global Int
+}
+
+// -----------------------------------------------------------------------------
 #define TIME 100000
 void _main (void)
 {
-    unsigned int ui;
+	unsigned int ui;
 
-    volatile unsigned int* cm_per_base = (unsigned int*)((char*)CM_PER_BASE+CM_PER_GPIO1);
-    volatile char* gpio_map = (char*)GPIO1_BASE;
-    volatile unsigned* gprev;
-    volatile unsigned* gpoe;
-    volatile unsigned* gpset;
-    volatile unsigned* gpclr;
+    *CM_DPLL_CLKSEL_TIMER2 = CLK_M_OSC;
 
-	gprev = (volatile unsigned*)(gpio_map+0x0000);
-    gpoe  = (volatile unsigned*)(gpio_map+0x134);
-    gpclr = (volatile unsigned*)(gpio_map+0x190);
-    gpset = (volatile unsigned*)(gpio_map+0x194);
+    *CM_PER_L3S = 0x2;
+    *CM_PER_TIMER2 = 0x2;
 
-    *cm_per_base = (1<<18 | 2);
+    *INTC_MIR2 &= 0xFFFFFFEF;
 
-    *gpoe &= ~(0xF<<21);
+    *DMTIMER2_IRQENABLE_SET = 0x2;
+    *DMTIMER2_TCRR = 0xFF000000;
+    *DMTIMER2_TCLR = 0x1;
+
+    *CM_PER_GPIO1 = (1<<18 | 0x2);
+
+    *GPIO1_OE &= ~(0xF<<21);
 
     while (1)
     {
-        *gpset = (0xF<<21);
+//        *GPIO1_SETDATAOUT = (0xF<<21);
         for(ui=0; ui<TIME; ui++);
-        *gpclr = (0xF<<21);
+//        *GPIO1_CLEARDATAOUT = (0xF<<21);
         for(ui=0; ui<TIME; ui++);
     }
 }
--- beagle/baremetal/start.s
+++ beagle/baremetal/start.s
@@ -1,11 +1,35 @@
 .global _start
 _start:
-    mrs r0, cpsr
-    bic r0, r0, #0x1F @ clear mode bits
-    orr r0, r0, #0x13 @ set SVC mode
-    orr r0, r0, #0xC0 @ disable FIQ and IRQ
-    ldr     sp,=stack_top
+    ldr r0, cpsr_svc
+    msr cpsr, r0
+    ldr sp,=stack_top
+
+    @ Set IVT base address in CP15 VBAR register
+    ldr r0, =0x4030CE00
+    mcr p15, 0, r0, c12, c0, 0  @Set VBAR
+
+    @ Assign the IRQ interrupt method
+    ldr r0, base_irq_addr
+    ldr r1, basic_handler
+    str r1,[r0]
+
+    @ Setup sp in IRQ mode
+    ldr r0, cpsr_irq
+    msr cpsr, r0
+    ldr sp,=irq_stack_top
+
+    @ Go back to supervisor mode, enable irq
+    ldr r0, cpsr_svc_ei
     msr cpsr, r0
 
     bl _main
     b .
+
+handle_irq:
+   bx lr
+
+cpsr_svc:      .word 0x193
+cpsr_irq:      .word 0x192
+cpsr_svc_ei:   .word 0x153
+base_irq_addr: .word 0x4030CE38
+basic_handler: .word irq_handler

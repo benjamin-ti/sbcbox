70f75fe48a2f734a04d4b7ba823c887e5e64c6bb
benjamin_ti <b.tisler@gmx.de>
2023-06-05 10:36:09 +0200

    kmfpga: Start new DMA-Transfer in Callback

--- cvoctavo/km_fpga/kmfpga.c
+++ cvoctavo/km_fpga/kmfpga.c
@@ -35,12 +35,7 @@ static struct dma_chan *dma_ch;
 
 static struct completion dma_m2m_ok;
 
-static void dma_memcpy_callback(void *data)
-{
-	printk("dma_memcpy_callback\n");
-
-	complete(&dma_m2m_ok);
-}
+static dma_addr_t  busSrc;
 
 static int kmmemcpy(void)
 {
@@ -113,6 +108,11 @@ volatile unsigned* gpoe;
 volatile unsigned* gpset;
 volatile unsigned* gpclr;
 
+dma_addr_t  busDestSet = (AM335X_GPIO_BASE+0x194);
+dma_addr_t  busDestClr = (AM335X_GPIO_BASE+0x190);
+
+unsigned int uiDMACounter = 0;
+
 static int gpioHardcore(void)
 {
     gpio_map = ioremap(AM335X_GPIO_BASE, 4096);
@@ -133,7 +133,7 @@ static int gpioHardcore(void)
 
     *gpset = 0x00000080;
     printk("Set\n");
-    usleep_range(2000*1000, 2000*1000);
+    usleep_range(1000*1000, 1000*1000);
     *gpclr = 0x00000080;
     printk("Del\n");
 
@@ -142,10 +142,128 @@ static int gpioHardcore(void)
     return 0;
 }
 
+static void dma_clrgpio_callback(void *data);
+
+static void dma_memcpy_callback(void *data)
+{
+	printk("dma_memcpy_callback\n");
+	complete(&dma_m2m_ok);
+}
+
+static void dma_setgpio_callback(void *data)
+{
+	struct dma_async_tx_descriptor *dma_m2m_desc;
+	dma_cookie_t cookie;
+
+	printk("dma_setgpio_callback\n");
+
+	uiDMACounter++;
+
+    dma_m2m_desc = dma_ch->device->device_prep_dma_memcpy(dma_ch,
+    		busDestClr, busSrc, 0x4, 0);
+    if (!dma_m2m_desc) {
+    	printk("device_prep_dma_memcpy failed\n");
+    	return;
+    }
+    dma_m2m_desc->callback = dma_clrgpio_callback;
+
+    cookie = dmaengine_submit(dma_m2m_desc);
+    if (dma_submit_error(cookie)){
+        printk(KERN_INFO "Failed to do DMA tx_submit");
+        return;
+    }
+
+    dma_async_issue_pending(dma_ch);
+}
+
+static void dma_clrgpio_callback(void *data)
+{
+	struct dma_async_tx_descriptor *dma_m2m_desc;
+	dma_cookie_t cookie;
+
+	printk("dma_clrgpio_callback\n");
+
+	if (uiDMACounter >= 10)
+	{
+		complete(&dma_m2m_ok);
+	}
+	else
+	{
+	    dma_m2m_desc = dma_ch->device->device_prep_dma_memcpy(dma_ch,
+	    		busDestSet, busSrc, 0x4, 0);
+	    if (!dma_m2m_desc) {
+	    	printk("device_prep_dma_memcpy failed\n");
+	    	return;
+	    }
+	    dma_m2m_desc->callback = dma_setgpio_callback;
+
+	    cookie = dmaengine_submit(dma_m2m_desc);
+	    if (dma_submit_error(cookie)){
+	        printk(KERN_INFO "Failed to do DMA tx_submit");
+	        return;
+	    }
+
+	    dma_async_issue_pending(dma_ch);
+	}
+}
+
+/*
+static int dma_init(void)
+{
+	dma_cap_mask_t mask;
+	struct dma_slave_config dma_cfg;
+	struct dma_async_tx_descriptor *dma_m2m_desc;
+	dma_cookie_t cookie;
+
+	int ret;
+
+    dma_cap_zero(mask);
+    dma_cap_set(DMA_MEMCPY, mask);
+    dma_ch = dma_request_channel(mask, NULL, NULL);
+    if (dma_ch == NULL)
+    {
+        printk("request_channel failed.\n");
+        return -1;
+    }
+
+    printk("got dma_ch = 0x%x\n", (unsigned int)dma_ch);
+
+    memset(&dma_cfg, 0, sizeof(struct dma_slave_config));
+    dma_cfg.direction = DMA_MEM_TO_MEM;
+    dma_cfg.dst_addr_width = DMA_SLAVE_BUSWIDTH_4_BYTES;
+    ret = dmaengine_slave_config(dma_ch, &dma_cfg);
+    if (ret) {
+    	printk("dmaengine_slave_config error\n");
+       goto err;
+    }
+    printk("dmaengine_slave_config pass\n");
+
+    if (dma_ch->device->device_prep_dma_memcpy == NULL)
+    {
+        printk("device_prep_dma_memcpy = NULL\n");
+        goto err;
+    }
+
+    dma_m2m_desc = dma_ch->device->device_prep_dma_memcpy(dma_ch,
+    								busDest, busSrc, len, 0);
+    if (!dma_m2m_desc) {
+    	printk("device_prep_dma_memcpy failed\n");
+    	goto err;
+    }
+    dma_m2m_desc->callback = dma_memcpy_callback;
+
+    cookie = dmaengine_submit(dma_m2m_desc);
+//    cookie = dma_m2m_desc->tx_submit(dma_m2m_desc); //submit the desc
+    if (dma_submit_error(cookie)){
+        printk(KERN_INFO "Failed to do DMA tx_submit");
+        goto err;
+    }
+}
+*/
+
 static int kmhello_init(void)
 {
     u32* src;
-    dma_addr_t  busSrc;
     u32* dest;
     dma_addr_t  busDest;
 
@@ -171,7 +289,7 @@ static int kmhello_init(void)
     //dest = kzalloc(len, GFP_KERNEL | GFP_DMA);
     //dest = dma_alloc_coherent(NULL, len, &busDest, GFP_KERNEL);
     dest = gpset;
-    busDest = (AM335X_GPIO_BASE+0x194);
+    busDest = busDestSet;
 
     for (i=0; i<len/4; i++)
     {
@@ -249,6 +367,31 @@ static int kmhello_init(void)
 
     printk("wait for dma fin\n");
     dma_async_issue_pending(dma_ch);
+    wait_for_completion(&dma_m2m_ok);
+
+    usleep_range(1000*1000, 1000*1000);
+    *gpclr = 0x00000080;
+    usleep_range(2000*1000, 2000*1000);
+
+    // 2. DMA attempt
+    dma_m2m_desc = dma_ch->device->device_prep_dma_memcpy(dma_ch,
+    		busDestSet, busSrc, len, 0);
+    if (!dma_m2m_desc) {
+    	printk("device_prep_dma_memcpy failed\n");
+    	goto err;
+    }
+    dma_m2m_desc->callback = dma_setgpio_callback;
+    // dma_m2m_desc->next = NULL;
+
+    cookie = dmaengine_submit(dma_m2m_desc);
+    if (dma_submit_error(cookie)){
+        printk(KERN_INFO "Failed to do DMA tx_submit");
+        goto err;
+    }
+
+    printk("wait for dma2 fin\n");
+    dma_async_issue_pending(dma_ch);
+
     wait_for_completion(&dma_m2m_ok);
 
     printk("src: %x\n", src[0]);

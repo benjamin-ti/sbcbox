22c18c4819689de1f71b75b1c02ad5914332aa9d
benjamin_ti <b.tisler@gmx.de>
2023-06-11 23:50:15 +0200

    Better TmpSvn for Uboot/linux in Buildroot

--- beagle/buildroot/bbbrcfg/patch_buildroot.sh
+++ beagle/buildroot/bbbrcfg/patch_buildroot.sh
@@ -4,5 +4,7 @@ brcfg_dir=$(realpath -s $(dirname "$0"))
 
 cd $brcfg_dir/../buildroot-2020.02.8/
 patch -p0 < $brcfg_dir/patches/buildroot/TmpSvnRepo4UbootLinux.patch
+patch -p0 < $brcfg_dir/patches/buildroot/SvnRepoSingelCheckIn.patch
+patch -p0 < $brcfg_dir/patches/buildroot/SvnRepoIgnore4Uboot.patch
 
 echo "do export CREATE_SVN_REPOS=1 to create local linux and uboot svn repos for better patch creation"

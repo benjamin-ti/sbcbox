a4e317520120a51022f71c5752ec6495a0f5d422
benjamin_ti <b.tisler@gmx.de>
2023-07-19 12:07:55 +0200

    Buildroot-Support in cvoctavo for km_fpga

--- beagle/buildroot/bbbrcfg/patch_buildroot.sh
+++ beagle/buildroot/bbbrcfg/patch_buildroot.sh
@@ -6,5 +6,6 @@ cd $brcfg_dir/../buildroot-2020.02.8/
 patch -p0 < $brcfg_dir/patches/buildroot/TmpSvnRepo4UbootLinux.patch
 patch -p0 < $brcfg_dir/patches/buildroot/SvnRepoSingelCheckIn.patch
 patch -p0 < $brcfg_dir/patches/buildroot/SvnRepoIgnore4Uboot.patch
+patch -p0 < $brcfg_dir/patches/buildroot/SvnRepoIgnore4Linux.patch
 
 echo "do export CREATE_SVN_REPOS=1 to create local linux and uboot svn repos for better patch creation"
